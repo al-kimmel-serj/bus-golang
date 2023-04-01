092ccd86a20ac2cf461ef71b8bd328fb90980159
Sergei Kimmel <al.kimmel.serj@gmail.com>
2023-04-01 23:29:32 +0400

    Initial commit.

---
+++ domain.go
@@ -0,0 +1,35 @@
+package bus
+
+import (
+	"google.golang.org/protobuf/proto"
+)
+
+type (
+	EventFilter       string
+	EventKey          string
+	EventName         string
+	EventVersion      int
+	PublisherEndpoint string
+)
+
+type Event[Payload proto.Message] struct {
+	EventKey     EventKey
+	EventPayload Payload
+}
+
+type Publisher[Payload proto.Message] interface {
+	Publish(events []Event[Payload]) error
+	Stop() error
+}
+
+type PublishersRegistry interface {
+	Register(eventName EventName, eventVersion EventVersion, host string, port int) (unregister func() error, err error)
+	Watch(eventName EventName, eventVersion EventVersion, handler func([]PublisherEndpoint)) (stop func() error, err error)
+}
+
+type Subscriber[Payload proto.Message] interface {
+	EventsChan() <-chan Event[Payload]
+	Stop() error
+	Subscribe(eventFilter EventFilter) error
+	Unsubscribe(eventFilter EventFilter) error
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/al-kimmel-serj/bus-golang
+
+go 1.20
+
+require google.golang.org/protobuf v1.30.0
